c49cc778d1734709e2a2e2f0ad17440e450b3e5b
b00974864 <foley-a6@ulster.ac.uk>
2025-11-10 16:29:27 +0000

    help

--- ConsoleApp1/User.cs
+++ ConsoleApp1/User.cs
@@ -1,13 +1,99 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace real
+namespace smarthomecontrollers
 {
-    internal class User
-
+    public class User
     {
+        // private fields 
+        private int userId;
+        private string username;
+        private string password;
+        private string contactInfo;
+        private bool isLoggedin;
+        private bool isloggedout; 
+        public int UserId
+        {
+            get { return UserId; }
+            set { UserId = value; }
+        }
+      
+        public string UserName
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        public string ContactInfo
+        {
+            get { return contactInfo; }
+            set { contactInfo = value; }
+        }
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+        public bool IsLoggedin
+        {
+            get { return isLoggedin; }
+            set { isLoggedin = value; }
+        }
+        public bool IsLoggedOut
+        {
+            get { return IsLoggedOut; } 
+            set { IsLoggedOut = value;  }
+            }
+
+
+        // constructor 
+
+        public User (int userID, 
+            string password,
+            string username,
+            string contactInfo,
+            bool isLoggedin)
+        {
+            userID = userId; 
+           UserName = username;
+            Password = password;
+            ContactInfo = contactInfo;
+           IsLoggedin = isLoggedin;
+        }
+
+    public bool Login( string username, string password )
+        {
+            if (UserName == UserName && Password == password )
+            {
+                isLoggedin = true;
+                Console.WriteLine("Log in Succesful");
+            }
+            else
+            {
+                isLoggedin = false;
+                Console.WriteLine($"Log in failed. User {username} or password is incorrect"); ; 
+
+            }
+            return isLoggedin;
+        }
+        public bool Logout( string username, string password ) {
+            if (isLoggedin == true )
+            {
+                  
+                isLoggedin = false;
+                Console.WriteLine("You have been logged out");
+            }
+            else
+            {
+                
+                isLoggedin = false;
+                Console.WriteLine("You are not logged in");
+            }
+            return IsLoggedin; 
+            }
     }
+
 }
